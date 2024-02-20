643a257056154ba6abcd62fcf53258e6708a7875
Hugo Martinez <le19080710@itmerida.edu.mx>
2024-02-19 22:09:21 -0600

    Refactor video list and add video 3 and 4

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,11 @@
 <template>
   <div class="container">
     <div class="row">
-
-      <VideoPlayer :videoEmbedUrl="selectedVideo ? selectedVideo.embedUrl : ''" />
+      <VideoPlayer
+        :videoEmbedUrl="selectedVideo ? selectedVideo.embedUrl : ''"
+      />
       <TextInput v-model="text" />
       <SubmitButton @submit="submit" />
-
     </div>
   </div>
 </template>
@@ -29,11 +29,25 @@ export default {
           id: 1,
           title: "Video 1",
           embedUrl: "https://www.youtube.com/watch?v=NgWfVITgPnI",
+        },
+        {
           id: 2,
           title: "Video 2",
-          embedUrl: "https://www.youtube.com/embed/fsxVo6WqI8w?si=utBEkBHGResAe0Zi",
+          embedUrl:
+            "https://www.youtube.com/embed/fsxVo6WqI8w?si=utBEkBHGResAe0Zi",
         },
-        // Agrega más videos según sea necesario
+        {
+          id: 3,
+          title: "Video 3",
+          embedUrl:
+            "https://www.youtube.com/embed/IPa-RRuUNaA?si=Ra6KJxV1-bj5jtod",
+        },
+        {
+          id: 4,
+          title: "Video 4",
+          embedUrl:
+            "https://www.youtube-nocookie.com/embed/n768i9tfFOs?si=KzR-6iGROLQcu466",
+        },
       ],
       selectedVideo: {
         id: 1,
@@ -48,16 +62,27 @@ export default {
       this.selectedVideo = video;
     },
     submit() {
-      console.log("Texto enviado:", this.text);
+    console.log("Texto enviado:", this.text);
 
-      const currentIndex = this.videoList.findIndex(video => video.id === this.selectedVideo.id);
-      const nextIndex = (currentIndex + 1) % this.videoList.length;
+    const currentIndex = this.videoList.findIndex(
+      (video) => video.id === this.selectedVideo.id
+    );
+    const nextIndex = (currentIndex + 1) % this.videoList.length;
 
+    if (nextIndex === 0) {
+      // Si nextIndex es 0, significa que hemos llegado al final de la lista
+      // Aquí puedes mostrar un mensaje indicando que el usuario ha visto todos los videos
+      alert("¡Has visto todos los videos!");
+      // También puedes reiniciar la lista de videos si quieres que comience desde el principio nuevamente
+      // this.selectedVideo = this.videoList[0];
+    } else {
+      // Si no es el último video, simplemente pasa al siguiente
       this.selectedVideo = this.videoList[nextIndex];
+    }
 
-      // Reinicia el texto si es necesario
-      this.text = " ";
-    },
+    // Opcional: Reinicia el texto solo si es necesario
+    this.text = '';
+  },
   },
 };
 </script>
